9ddcd18d87b6e3a42b7c794ab8e8a210ad04ded4
bellahj0323 <33356875+bellahj0323@users.noreply.github.com>
2019-10-25 21:41:39 +0900

    Create dataload.py

---
+++ dataload.py
@@ -0,0 +1,72 @@
+import os
+import cv2
+import numpy as np
+import pandas as pd
+from PIL import Image
+
+class Dataset:
+  def __init__(self, directory, batch_size, batch_per_video, seq, offset):
+    self.directory = directory
+    self.videos = [os.path.join(self.directory, i) for i in os.listdir(directory)]
+                                               # list of videos ex) Train001, Train002, ...
+    self.batch_size = batch_size
+    self.batch_per_video = batch_per_video
+    self.seq = seq  # list
+    self.offset = offset 
+    
+  def _load_fraem(self, frame_path):
+    frame = Image.open(frame_path)
+    arr = np.array(frame)
+    arr = np.expand_dims(arr, -1) # (158, 238, 1)
+    arr = arr.astype('float32')
+    return arr
+    
+  def random_frames(self):
+    video_idx = np.random.randint(low=0, high=len(self.videos))
+    video = self.videos[video_idx]
+    # video 선택 했으니까 frame np.array로 불러오기
+    video_len = int(len(os.listdir(video))) - 2 - offset - seq[-1]
+    frames = [os.path.join(video, '%03d.tif' %i) for i in range(video_len)]
+    
+    idx_y = np.random.randint(self.offset, len(frames), self.batch_per_video)
+    idx_x = [[y - self.offset + x for x in self.seq] for y in idx_y]
+    
+    frame_y = np.array([self._load_frame(frames[i]) for i in idx_y])
+    frame_x = []
+    for x in zip(*idx_x):
+      temp = np.array([self._load_frame(frames[i]) for i in x])
+      frame_x.append(temp_x)
+      
+    return frame_x, frame_y
+    
+  def train_loader(self):
+    while True:
+      for i in range(int(self.batch_size/self.batch_per_video)):
+        x, y = self.random_frames()
+        if i == 0:
+          batch_x = x
+          batch_y = y
+        else:
+          batch_x = np.concatenate((batch_x, x), axis=1)
+          batch_y = np.concatenate((batch_y, y), axis=0)
+         
+      batch_x = list(batch_x)
+      yield batch_x, batch_y
+      
+  def test_loader(self, video_idx):
+    video = self.videos[video_idx]
+    # video 선택 했으니까 frame np.array로 불러오기    
+    video_len = int(len(os.listdir(video))) - 2 - offset - seq[-1]
+    frames = [op.path.join(video, '%03d.tif' %i) for i in range(video_len)]
+    
+    idx_y = np.array(self.offset , len(frames))
+    idx_x = [[y - self.offset + x for x in seq] for y in idx_y]
+    
+    frame_y = np.array([self._load_frame(frames[i]) for i in idx_y])
+    frame_x = []
+    for x in zip(*idx_x):
+      temp = np.array([self._load_frame(frames[i]) for i in x])
+      frame_x.append(temp)
+      
+    return frame_x, frame_y
+  
