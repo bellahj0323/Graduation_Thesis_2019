eee272d064bbd4968295002c5e84ab29fb28b7c6
bellahj0323 <33356875+bellahj0323@users.noreply.github.com>
2019-10-25 21:47:42 +0900

    Update model.py

--- model.py
+++ model.py
@@ -6,19 +6,23 @@ from keras.layers.normalization import BatchNormalization
 # (n_frames, rows, cols, channels) input shape
 # (None, 158, 238, 1)
 
-model = Sequential()
+def ConvLSTM():
 
-model.add(Conv3D(filters=10, kernel_size=(3,3,3)
-                 ,strides=2, padding='same', data_format='channels_last'
-                 ,activation='relu', kernel_initializer='he_normal'
-                ,input_shape=(None,158,238,1)))
+  model = Sequential()
 
-model.add(ConvLSTM2D(filters=32, kernel_size=(3,3)
-                     ,activation='relu',data_format='channels_last'
-                     ,padding='same',return_sequences=False))
-                     
-model.add(BatchNormalization())
+  model.add(Conv3D(filters=10, kernel_size=(3,3,3)
+                   ,strides=2, padding='same', data_format='channels_last'
+                   ,activation='relu', kernel_initializer='he_normal'
+                  ,input_shape=(None,158,238,1)))
 
-model.add(Conv2DTranspose(filters=1, kernel_size=(3,3)
-                          ,strides=2, padding='same', data_format='channels_last'
-                          ,activation='relu',kernel_initializer='he_normal'))
+  model.add(ConvLSTM2D(filters=32, kernel_size=(3,3)
+                       ,activation='relu',data_format='channels_last'
+                       ,padding='same',return_sequences=False))
+
+  model.add(BatchNormalization())
+
+  model.add(Conv2DTranspose(filters=1, kernel_size=(3,3)
+                            ,strides=2, padding='same', data_format='channels_last'
+                            ,activation='relu',kernel_initializer='he_normal'))
+  
+  return model
