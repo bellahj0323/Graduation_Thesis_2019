8623c2b28448c8397cb42abe5e25fbd007088618
bellahj0323 <33356875+bellahj0323@users.noreply.github.com>
2019-11-17 22:03:08 +0900

    Update model.py

--- model.py
+++ model.py
@@ -4,7 +4,7 @@ from keras.layers.convolutional_recurrent import ConvLSTM2D
 from keras.layers.normalization import BatchNormalization
 
 # (n_frames, rows, cols, channels) input shape
-# (None, 158, 238, 1) -> resized (None, 128, 128, 1)
+# (None, 158, 238, 1)
 
 def ConvLSTM(optimizer):
 
@@ -12,7 +12,7 @@ def ConvLSTM(optimizer):
 
   model.add(ConvLSTM2D(filters=20, kernel_size=(3,3), strides=1, padding='same', activation='relu',
                         kernel_initializer='he_normal',data_format='channels_last',
-                        input_shape=(None, 128, 128, 1), return_sequences=True))
+                        input_shape=(None, 158, 238, 1), return_sequences=True))
 
   model.add(ConvLSTM2D(filters=1, kernel_size=(3,3), strides=2, padding='same', activation='relu',
                      data_format='channels_last', kernel_initializer='he_normal', return_sequences=False))
