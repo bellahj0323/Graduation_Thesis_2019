b76c048aa81cc2b21ee0e61e72dd554c8079d6d6
bellahj0323 <33356875+bellahj0323@users.noreply.github.com>
2019-11-17 22:01:35 +0900

    Update model.py

--- model.py
+++ model.py
@@ -10,12 +10,18 @@ def ConvLSTM(optimizer):
 
   model = Sequential()
 
-  
-  model.add(ConvLSTM2D(strides=2, padding='same', activation='relu',
-                        kernel_initializer='he_normal', kernel_size=(3,3),
-                        input_shape=(None, 158, 238, 1), return_sequences=False))
+  model.add(ConvLSTM2D(filters=20, kernel_size=(3,3), strides=1, padding='same', activation='relu',
+                        kernel_initializer='he_normal',data_format='channels_last',
+                        input_shape=(None, 128, 128, 1), return_sequences=True))
+
+  model.add(ConvLSTM2D(filters=1, kernel_size=(3,3), strides=2, padding='same', activation='relu',
+                     data_format='channels_last', kernel_initializer='he_normal', return_sequences=False))
 
   model.add(BatchNormalization())
+  
+  model.add(Conv2DTranspose(filters=1, kernel_size=(3,3)
+                          ,strides=2, padding='same', data_format='channels_last'
+                          ,activation='relu',kernel_initializer='he_normal'))
   
   model.compile(optimizer=optimizer, loss='mean_squared_error')
   
