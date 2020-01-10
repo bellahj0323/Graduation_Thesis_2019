56f3cdd13daddf1c08935377e0de429896e844d9
Chae hj <julie-.-@hanmail.net>
2020-01-10 18:48:04 +0900

    Checkpoint and Earlystopping as callbacks

--- main.py
+++ main.py
@@ -82,8 +82,9 @@ def make_pred_video(pred):
     
 
 def train(dataload, validation, model, epochs, steps_per_epoch, save_path):
-  checkpoint = ModelCheckpoint('{}.h5'.format(save_path), monitor='val_loss', verbose=1, save_best_only=True, mode='min')
-  callbacks_list = [checkpoint]
+  checkpoint = ModelCheckpoint('{}.h5'.format(save_path), monitor='loss', verbose=1, save_best_only=True, mode='min')
+  #callbacks_list = [checkpoint]
+  early_stopping = EarlyStopping(monitor='val_loss', patience=2, verbose=1)
   
   history = model.fit_generator(
     generator=dataload,
@@ -91,7 +92,7 @@ def train(dataload, validation, model, epochs, steps_per_epoch, save_path):
     validation_steps=1,
     epochs = epochs,
     steps_per_epoch = steps_per_epoch,
-    callbacks = callbacks_list
+    callbacks = [checkpoint, early_stopping]
   )
   history_df = pd.DataFrame(history.history)
   history_df.to_csv('{}.csv'.format(save_path), index=False)
