611341d49a19d27b5ba4735531b4373729289610
Chae hj <julie-.-@hanmail.net>
2020-01-28 23:24:03 +0900

    Change score threshold 0.002 -> 0.003

--- main.py
+++ main.py
@@ -126,7 +126,7 @@ def abnormal_test(pred, real):
     detect = np.zeros(len(score))
 
     for i in range(len(score)):
-        if(score[i] > 0.002):
+        if(score[i] > 0.003):
             detect[i] = 1
             
     return abnormal, score, detect
